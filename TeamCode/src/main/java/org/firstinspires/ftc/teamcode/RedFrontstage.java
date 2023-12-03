add14f50e55f6f21d4da61953c20dee27be582a5
Aaron-FTC-Junior <114203462+Aaron-FTC-Junior@users.noreply.github.com>
2023-12-02 21:01:12 -0700

    Cleaned up Files

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RedFrontstage.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RedFrontstage.java
@@ -4,31 +4,20 @@ import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
-
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.firstinspires.ftc.teamcode.Vision.OpenCVRed;
-import org.firstinspires.ftc.teamcode.drive.DriveConstants;
 import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
 import org.firstinspires.ftc.teamcode.trajectorysequence.TrajectorySequence;
-import org.opencv.core.Mat;
-import org.opencv.core.Point;
-import org.opencv.core.Scalar;
-import org.opencv.imgproc.Imgproc;
 import org.openftc.easyopencv.OpenCvCamera;
 import org.openftc.easyopencv.OpenCvCameraFactory;
 import org.openftc.easyopencv.OpenCvCameraRotation;
-import org.openftc.easyopencv.OpenCvPipeline;
 import org.openftc.easyopencv.OpenCvWebcam;
 
 @Autonomous (name = "Red Frontstage")
 public class RedFrontstage extends LinearOpMode {
-
     OpenCvWebcam webcam;
-
     OpenCVRed pipeline = new OpenCVRed(telemetry);
     private DcMotor Arm, Extend = null;
-
-    private double ArmPower = 0.5, SlidePower = 0.4;
 
     @Override
     public void runOpMode()
@@ -43,28 +32,18 @@ public class RedFrontstage extends LinearOpMode {
         Extend.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         Extend.setDirection(DcMotor.Direction.REVERSE);
 
-
         int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
         webcam = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, "Webcam 1"), cameraMonitorViewId);
-
         webcam.setPipeline(pipeline);
-
         webcam.setMillisecondsPermissionTimeout(5000); // Timeout for obtaining permission is configurable. Set before opening.
         webcam.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener()
         {
             @Override
             public void onOpened()
-            {
-                webcam.startStreaming(1920, 1080, OpenCvCameraRotation.UPRIGHT);
-            }
+            {webcam.startStreaming(1920, 1080, OpenCvCameraRotation.UPRIGHT);}
 
             @Override
-            public void onError(int errorCode)
-            {
-                /*
-                 * This will be called if the camera could not be opened
-                 */
-            }
+            public void onError(int errorCode) {telemetry.clearAll(); telemetry.addLine("Camera failed to open");}
         });
 
         telemetry.addLine("Waiting for start");
